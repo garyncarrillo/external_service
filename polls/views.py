425ba390e14f431ed63e323a8bf335f49661513a
Garyn Carrillo Caballero <garync@gmail.com>
2022-09-10 11:49:49 -0500

    Add details view and behavion on endpoint

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 
@@ -7,13 +7,17 @@ from .models import Question
 def index(request):
     lastes_question_list = Question.objects.filter()
     # render(request=, "template", "context")
-    
+
     return render(request, "polls/index.html", {
         "lastes_question_list": lastes_question_list
     })
 
 def details(request, question_id):
-    return HttpResponse(f"Esta viendo la pregunta no {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    
+    return render(request, "polls/details.html", {
+        "question": question
+    })
 
 def results(request, question_id):
     return HttpResponse(f"Estas viendo los resultados de la pregunta no {question_id}")
