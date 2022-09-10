1172c29743a393daf142ad79e269382c29ac858f
Garyn Carrillo Caballero <garync@gmail.com>
2022-09-10 11:36:47 -0500

    Add template with data query

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<strong>Esta es la pagina principal de premios platzi</strong>")
+    lastes_question_list = Question.objects.filter()
+    # render(request=, "template", "context")
+    
+    return render(request, "polls/index.html", {
+        "lastes_question_list": lastes_question_list
+    })
 
 def details(request, question_id):
     return HttpResponse(f"Esta viendo la pregunta no {question_id}")
